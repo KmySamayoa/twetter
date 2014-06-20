ebabcf5e0232dca5a2a04f84be851e7449fba422
KmySamayoa <camila.samayoa@gmail.com>
2014-06-20 13:37:51 +0000

    worked on gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,3 +80,15 @@ module ApplicationHelper
     html.html_safe
   end
 end
+
+    
+def gavatar_url(user)
+  if user.gavatar_url.present?
+    user.gavatar_url
+  else
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  end
+end
+
